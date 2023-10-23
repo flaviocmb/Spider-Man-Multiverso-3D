7d556b91ba00ede04c20e5a25c1b613bb227843c
flaviocmb <flavio.cmb@gmail.com>
2023-10-23 10:29:28 -0300

    Carrossel rotativo adicionado

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,5 @@
 function handleMouseEnter() {
-    console.log(this);
+    // console.log(this);
     this.classList.add('s-card--hovered');
     document.body.id = `${this.id}-hovered`;
 }
@@ -37,3 +37,19 @@ function AddEventListenersToCards() {
 
 // DOMContentLoaded garante que o conteúdo do DOM vai ser carregado e só depois o JavaScript será executado
 document.addEventListener('DOMContentLoaded', AddEventListenersToCards, false);
+
+/* outra abordagem de utilização dos eventos de click sem um addEventListener, a desvantagem 
+é que será necessário declarar cada onclick no html */
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1) // 360/3 (3 cards)
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}Deg)`);
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.s-controller__button--active');
+    activeButtonElement.classList.remove('s-controller__button--active');
+    selectedButtonElement.classList.add('s-controller__button--active');
+}
